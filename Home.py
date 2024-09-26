fe920cc81d43617db3d68000d61cf2159a23b283
samourad <alisoule.mouradi@eemi.com>
2024-09-26 14:23:58 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,3 +10,6 @@ st.write("Ceci est un formulaire de contact")
 user_input=st.text_input("tapez votre texte : ")
 
 st.write(user_input)
+
+#Image
+st.image("https://www.google.com/imgres?q=eemi&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100063733442303&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Feemi75%2F&docid=liTig4DeeUiMMM&tbnid=Je8AhCyNeMtwLM&vet=12ahUKEwi786zUzOCIAxWJT6QEHQeAL_YQM3oECE0QAA..i&w=360&h=360&hcb=2&ved=2ahUKEwi786zUzOCIAxWJT6QEHQeAL_YQM3oECE0QAA")
