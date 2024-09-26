719aed1ede0d2d5416af89488370e4390e3aed21
samourad <alisoule.mouradi@eemi.com>
2024-09-26 14:42:39 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,3 +19,15 @@ st.sidebar.title("Ali Mouradi")
 
 #Vidéo dans la sidebar
 st.sidebar.video("https://youtu.be/VUkm6b58bMo")
+
+#select Bare
+student_grad = st.selectbox("Selectionnez votre niveau d'étude", ["Bac", "Bac+2", "Bac+5"])
+
+#Select slider
+age = st.select_slider("Quel est votre âge ?", range(0,99))
+
+if age > 18:
+  st.write("Vous êtes majeur")
+else:
+  st.write("Vous êtes mineur")
+
