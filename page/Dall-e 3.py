c32f4ae1755f19c39fdb37cc44a867f6408b9a86
samourad <alisoule.mouradi@eemi.com>
2024-09-26 15:43:32 +0200

    Update Dall-e 3.py

--- page/Dall-e 3.py
+++ page/Dall-e 3.py
@@ -19,6 +19,6 @@ Sidebare
 st.sidebar.title("intéraction avec openAI")
 
 #Image
-st.image("https://forums.getdrafts.com/uploads/default/original/2X/e/e7b4d0c64bccff6787857f8d940e8193b9ab9a90.jpeg")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXuDvlNDmDGF5QwPETEs3eh7RHNGmKBpgwyw&s")
 
 
