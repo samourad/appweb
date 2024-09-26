d37b8d9dc1613433a39312ec35514567f2a38218
samourad <alisoule.mouradi@eemi.com>
2024-09-26 15:38:30 +0200

    Update Dall-e 3.py

--- page/Dall-e 3.py
+++ page/Dall-e 3.py
@@ -1,2 +1,24 @@
 import streamlit as st
 st.title("Dall-E ici")
+
+#Titre
+st.title("Dall-e 3")
+
+#Texte
+st.write("Veuillez entre une description de votre texte que vous souhaitez générer")
+
+#Champ de saisi
+user_input=st.text_input("tapez votre texte : ")
+
+st.write(user_input)
+
+Sidebare
+st.sidebar.title("Assistant")
+
+Sidebare
+st.sidebar.title("intéraction avec openAI")
+
+#Image
+st.image("https://forums.getdrafts.com/uploads/default/original/2X/e/e7b4d0c64bccff6787857f8d940e8193b9ab9a90.jpeg")
+
+
